f6874a52a02a7b44b4155f5cf48139973a013427
周健邦 <3510537395@qq.com>
2023-07-03 18:42:03 +0800

    修复一些拼接url的bug

--- src/hooks/useUrl.ts
+++ src/hooks/useUrl.ts
@@ -1,6 +1,6 @@
 // 微信授权地址回调
 export const useUrl = () => {
-	const redirectUrl = import.meta.env.VITE_REDIRECT_URL
+	const redirectUrl = encodeURIComponent(import.meta.env.VITE_REDIRECT_URL)
 	const appId = import.meta.env.VITE_APP_ID
 	return `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${appId}&redirect_uri=${redirectUrl}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`
 }
