281743a20c83cdc96570bdeeb0afcae7b5035fed
周健邦 <3510537395@qq.com>
2023-07-04 22:48:00 +0800

    进出记录页面新增上拉刷新和下滑加载更多功能

--- src/stores/modules/record.ts
+++ src/stores/modules/record.ts
@@ -5,23 +5,45 @@ import type { Record } from '@/global/record'
 export const useRecordStore = defineStore(
 	'record',
 	() => {
+		// 当前页数
+		const currentPage = ref(1)
+		// 当前一页展示的数量
+		const pageSize = ref(10)
 		// 进出记录数据
 		const recordList = ref<Record.recordList[]>([])
 		// 记录总数
 		const total = ref(0)
+		// 是否获取完所有数据
+		const isGetAll = ref<any>(false)
+		// 是否获取数据中
+		const isLoading = ref(false)
 		// 选中的日期
 		const selectDate = ref('')
 		// 获取所有进出记录
-		async function getAllRecordAction(
-			currentPage: number,
-			pageSize: number,
-			params: Record.ReqGetAllRecord
-		) {
-			const { result } = await getAllRecord(currentPage, pageSize, params)
-			recordList.value = result.recordList
+		async function getAllRecordAction(params: Record.ReqGetAllRecord) {
+			if (isGetAll.value || isLoading.value) return
+			isLoading.value = true
+			const { result } = await getAllRecord(
+				currentPage.value,
+				pageSize.value,
+				params
+			)
+			recordList.value.push(...result.recordList)
 			total.value = result.total
+			if (result.recordList.length < pageSize.value) {
+				isGetAll.value = true
+			}
+			isLoading.value = false
 		}
-		return { recordList, total, selectDate, getAllRecordAction }
+		return {
+			recordList,
+			total,
+			selectDate,
+			currentPage,
+			pageSize,
+			isGetAll,
+			getAllRecordAction
+		}
 	},
 	{
 		persist: piniaPersistConfig('record', localStorage, ['selectDate'])
--- src/views/Record/index.vue
+++ src/views/Record/index.vue
@@ -9,6 +9,8 @@ const router = useRouter()
 
 // 日期选择器的显示
 const showPicker = ref(false)
+// 是否在触发上拉刷新
+const loading = ref(false)
 
 // 发送请求的params参数
 const searchForm = ref({
@@ -18,7 +20,7 @@ const searchForm = ref({
 onMounted(() => {
 	if (recordStore.selectDate === '') {
 		getNowFormatDate()
-		recordStore.getAllRecordAction(1, 100, searchForm.value)
+		recordStore.getAllRecordAction(searchForm.value)
 	}
 })
 
@@ -29,7 +31,7 @@ const onConfirm = ({ selectedValues }: any) => {
 	searchForm.value.startTime = new Date(
 		new Date(recordStore.selectDate).getTime()
 	)
-	recordStore.getAllRecordAction(1, 100, searchForm.value)
+	recordStore.getAllRecordAction(searchForm.value)
 }
 
 // 获取七天前的日期
@@ -73,50 +75,69 @@ const handleRecordClick = (item: Record.recordList) => {
 		}
 	})
 }
+
+// 上拉加载更多记录
+const getMoreRecord = () => {
+	recordStore.currentPage++
+	recordStore.getAllRecordAction(searchForm.value)
+}
+
+// 上拉刷新回调
+const onRefresh = () => {
+	recordStore.currentPage = 1
+	recordStore.isGetAll = false
+	recordStore.recordList = []
+	recordStore.getAllRecordAction(searchForm.value)
+	loading.value = false
+}
 </script>
 
 <template>
-	<!-- 回到顶部 -->
-	<van-back-top right="1vw" bottom="10vh" />
-	<!-- 总数 -->
-	<div class="text-center mt1.5 text-1.5 total">
-		共{{ recordStore.recordList.length }}条记录
-	</div>
-	<!-- 日期选择 -->
-	<van-field
-		v-model="recordStore.selectDate"
-		is-link
-		readonly
-		name="datePicker"
-		label="起始时间"
-		placeholder="点击选择时间"
-		@click="showPicker = true"
-	/>
-	<van-popup v-model:show="showPicker" position="bottom">
-		<van-date-picker @confirm="onConfirm" @cancel="showPicker = false" />
-	</van-popup>
-	<!-- 进出记录数据展示 -->
-	<div class="recordList">
-		<van-empty
-			description="暂无进出记录"
-			image-size="75"
-			v-if="recordStore.recordList.length === 0"
+	<van-pull-refresh v-model="loading" @refresh="onRefresh">
+		<!-- 回到顶部 -->
+		<van-back-top right="1vw" bottom="10vh" />
+		<!-- 总数 -->
+		<div class="text-center mt1.5 text-1.5 total">
+			共{{ recordStore.total }}条记录
+		</div>
+		<!-- 日期选择 -->
+		<van-field
+			v-model="recordStore.selectDate"
+			is-link
+			readonly
+			name="datePicker"
+			label="起始时间"
+			placeholder="点击选择时间"
+			@click="showPicker = true"
 		/>
-		<template v-else>
-			<div
-				class="flex justify-between m2 p2 text-1.5 text-dark bg-#ddd b-rd"
-				v-for="item in recordStore.recordList"
-				:key="item.recordId"
-				@click="handleRecordClick(item)"
-			>
-				<van-space direction="vertical" fill>
-					<span>姓名：{{ item.cardName }}</span>
-					<span>班级：{{ getClassName(item.user.unitsName) }}</span>
-					<span>刷脸时间：{{ formattedDate(item.stuTime) }}</span>
-				</van-space>
-			</div>
-		</template>
-	</div>
+		<van-popup v-model:show="showPicker" position="bottom">
+			<van-date-picker @confirm="onConfirm" @cancel="showPicker = false" />
+		</van-popup>
+		<!-- 进出记录数据展示 -->
+		<div class="recordList">
+			<van-empty
+				description="暂无进出记录"
+				image-size="75"
+				v-if="recordStore.recordList.length === 0"
+			/>
+			<template v-else>
+				<van-list @load="getMoreRecord">
+					<div
+						class="flex justify-between m2 p2 text-1.5 text-dark bg-#ddd b-rd"
+						v-for="item in recordStore.recordList"
+						:key="item.recordId"
+						@click="handleRecordClick(item)"
+					>
+						<van-space direction="vertical" fill>
+							<span>姓名：{{ item.cardName }}</span>
+							<span>班级：{{ getClassName(item.user.unitsName) }}</span>
+							<span>刷脸时间：{{ formattedDate(item.stuTime) }}</span>
+						</van-space>
+					</div>
+				</van-list>
+			</template>
+		</div></van-pull-refresh
+	>
 </template>
 
 <style scoped>
